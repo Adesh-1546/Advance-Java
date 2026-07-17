15a7f99e5d0ba019530a4334bbf0adad9258dd6e
Adesh Pol <adeshpol1234@gmail.com>
2026-07-17 09:35:54 +0530

    Create ResultSetExample.java

---
+++ 02-JDBC/05-ResultSet/ResultSetExample.java
@@ -0,0 +1,42 @@
+package JDBC;
+
+import java.sql.*;
+
+public class ResultSetExample {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:postgresql://localhost:5432/testdb";
+        String username = "postgres";
+        String password = "password";
+
+        String query = "SELECT * FROM student";
+
+        try (Connection con = DriverManager.getConnection(url, username, password);
+             PreparedStatement ps = con.prepareStatement(query);
+             ResultSet rs = ps.executeQuery()) {
+
+            System.out.println("Fetching data...");
+
+            // 🔹 Iterating ResultSet
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+
+                System.out.println("ID: " + id + ", Name: " + name);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/*
+Expected Output:
+
+Fetching data...
+ID: 1, Name: Adesh
+ID: 2, Name: Rahul
+...
+*/
